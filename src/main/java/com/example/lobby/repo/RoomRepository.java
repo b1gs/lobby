539b1a1a80bfa14573bb97ec2ba5fdd92532f2e3
oleksandr.volkovsky <oleksandr.volkovsky@smava.de>
2018-02-01 15:07:57 +0200

    Yum file was added.

--- src/main/java/com/example/lobby/repo/RoomRepository.java
+++ src/main/java/com/example/lobby/repo/RoomRepository.java
@@ -4,11 +4,13 @@ import com.example.lobby.domain.Room;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface RoomRepository {
 
-    @Query("SELECT a from Room r WHERE active = true")
+    @Query("SELECT a from Room r ")
     List<Room> findAllActive();
 
 }
