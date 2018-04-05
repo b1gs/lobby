14ea6acfcd68fb0498d0d039abac7f245383db07
ovolkovskyi <oleksandr.volkovsky@smava.de>
2018-04-05 19:07:58 +0300

    Added database support with dummy tables and dummy data.

---
+++ src/main/resources/db/data/V1_001__initializeData.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO users(
+            id, username, email, rank, creation_date, last_login)
+    VALUES (nextval('id_seq'), 'b1gs','b1gsmen@b1gs', 0, current_date, current_date);
+
+INSERT INTO users(
+            id, username, email, rank, creation_date, last_login)
+    VALUES (nextval('id_seq'), 'toxic','toxic@b1gs', 0, current_date, current_date);
+
+INSERT INTO users(
+            id, username, email, rank, creation_date, last_login)
+    VALUES (nextval('id_seq'), 'fred','fred@b1gs', 0, current_date, current_date);
+
+INSERT INTO users(
+            id, username, email, rank, creation_date, last_login)
+    VALUES (nextval('id_seq'), 'albert','albert@b1gs', 0, current_date, current_date);
+
+INSERT INTO users(
+            id, username, email, rank, creation_date, last_login)
+    VALUES (nextval('id_seq'), 'eddy','eddy@b1gs', 0, current_date, current_date);
+
+
+INSERT INTO room(
+            id, room_name)
+    VALUES (nextval('id_seq'), 'DefaultRoom1');
+
+INSERT INTO room(
+            id, room_name)
+    VALUES (nextval('id_seq'), 'DefaultRoom2');
+
+INSERT INTO room(
+            id, room_name)
+    VALUES (nextval('id_seq'), 'DefaultRoom2');
--- src/main/resources/db/migration/V1_000__initializeTables.sql
+++ src/main/resources/db/migration/V1_000__initializeTables.sql
@@ -1,21 +1,18 @@
 
-CREATE TABLE user {
+CREATE SEQUENCE id_seq START 1 INCREMENT 50;
 
+CREATE TABLE users (
   id bigint NOT NULL,
-  username character varying(255) NOT NULL ,
+  username character varying(255) NOT NULL,
   email character varying(255),
   rank integer,
   creation_date timestamp with time zone,
   last_login timestamp with time zone,
   CONSTRAINT "user_pk" PRIMARY KEY (id)
+);
 
-}
-
-
-CREATE TABLE room {
-
+CREATE TABLE room (
   id bigint NOT NULL,
   room_name character varying(255) NOT NULL,
   CONSTRAINT "room_pk" PRIMARY KEY (id)
-
-}
+);
